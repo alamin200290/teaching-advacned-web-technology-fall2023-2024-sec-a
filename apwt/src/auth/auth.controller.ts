77b781c1850556123f8996bf854df95495816b88
alamin <amin.se.aiub@gmail.com>
2023-10-31 19:50:45 +0600

    into guard

--- apwt/src/auth/auth.controller.ts
+++ apwt/src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthGuard } from "./auth.guard";
 
 @Controller('auth')
 export class AuthController{
 
     constructor(private AuthService: AuthService){}
 
+    @UseGuards(AuthGuard)
     @Get('signup')
     signup(){
         return this.AuthService.signup();
