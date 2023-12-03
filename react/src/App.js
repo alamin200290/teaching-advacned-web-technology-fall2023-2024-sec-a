eb599c88b63feaa4a108968d333551a9a056247f
alamin <amin.se.aiub@gmail.com>
2023-12-03 20:33:11 +0600

    intro react hook

--- react/src/App.js
+++ react/src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import Test from './Test';
 import Btn from './Btn';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  
+  useEffect(()=>{
+    alert('Effect: test');
+  }, [count]);
   
   function abc() {
     alert('test');
+  }
+
+  function getLike(){
+    setCount(count+1);
+  }
+
+  function dislike(){
+    setCount(count-1);
   }
 
   return (
     <div>
       <Test name='alamin'/>
       <Btn name='abc' events={abc} />
-      <button onClick={()=>{abc()}}>Click</button>
+      <button onClick={getLike}>Like</button>
+      <h1>{count}</h1>
+      <button onClick={dislike}>dislike</button>
     </div>
   );
 }
